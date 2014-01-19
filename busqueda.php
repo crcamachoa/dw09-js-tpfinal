9d730e2f540d43ec07da9184aaff9d7bd4c814e2
Alain Loup <alainloup@gmail.com>
2014-01-19 20:22:25 -0300

    Conflictos entre servicios y lista

--- busqueda.php
+++ busqueda.php
@@ -8,119 +8,106 @@ $rec_limit = $contactos->getRowPage(); //obtener lineas por pagina
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-<!-- 	<link   href="css/bootstrap.min.css" rel="stylesheet"> -->
-<!-- 	<script src="js/bootstrap.min.js"></script> -->
-<!-- 	<script type="text/javascript" src="js/jquery.js"></script> -->
-  <meta charset="utf-8">
+	<meta charset="utf-8">
+	<!-- 	<link   href="css/bootstrap.min.css" rel="stylesheet"> -->
+	<!-- 	<script src="js/bootstrap.min.js"></script> -->
+	<!-- 	<script type="text/javascript" src="js/jquery.js"></script> -->
+	<meta charset="utf-8">
 	<link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
 	<script type="text/javascript" src="js/jquery.js"></script> 
+	<script type="text/javascript" src="js/jquery-1.10.1.js"></script>
 	<script type="text/javascript" src="js/bootstrap.js"></script>
+
 </head>
- 
+
 <body>
 	<?php include 'menu.php'; ?>
-    <div class="container">
-    		<br><br><br>
-        <div class="row">
-        	<h3>Contactos</h3>
-        </div>
-        <div class="row">
-        	<div id="servicioListaContainer" class="col-md-2">
-          	<div class="list-group">
-						  <a href="#" class="list-group-item active">Cras justo odio</a>
-						  <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
-						  <a href="#" class="list-group-item">Morbi leo risus</a>
-						  <a href="#" class="list-group-item">Porta ac consectetur ac</a>
-						  <a href="#" class="list-group-item">Vestibulum at eros</a>
-						</div>
-        	</div>
-        	<div id="contactoTablaContainer" class="col-md-8">
-          	<table id="tableFilter" class="table table-striped table-bordered">
-<!--                 <table id="tableFilter" class="table table-striped table-hover table-condensed"> -->
-                  <thead>
-                    <tr>
-                      <th>ID</th>
-                      <th>NOMBRE</th>
-                      <th>APELLIDO</th>
-                      <th>EMAIL</th>
-                      <th>CI</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                  <?php         
-				            /* Obtener el total de registros */
-				            //$cantidad = $contactos->count_where();
-                  	$cantidad = $contactos->count();
-				            $cantidad = $cantidad['cantidad'];
-				            
-				            if (isset($_GET{'page'})) { //get the current page
-				            	$page = $_GET{'page'};
-				            } else {
-				            	// show first set of results
-				            	$page = 1;
-				            }
-	            	
-				            $data = $contactos->get_contactos_page($page - 1);
-				            //$data = $contactos->get_contactos_where_page($where,$page - 1);
-				            $total_pag = ceil($cantidad / $rec_limit);
-				            
-				            if ($page > $total_pag){
-				            	header("Location: $_PHP_SELF?page=$total_pag");
-				            }
-				            
-				            foreach($data as $fila){
-												echo '<tr>';
-												echo '<td>'. $fila['id'] . '</td>';
-												echo '<td>'. $fila['nombre'] . '</td>';
-												echo '<td>'. $fila['apellido'] . '</td>';
-												echo '<td>'. $fila['email'] . '</td>';
-												echo '<td>'. $fila['ci'] . '</td>';
-												echo '</tr>';
-										}
-				            
-				            echo '</tr>';
-				            echo '</tbody>';
-				            echo '</table>';
-				            echo "<div class=\"text-center\">";
-				            //empieza paginacion
-				            $page = ($page == 0 ? 1 : $page);
-				            $prev = ($page == 1 ? 1 : $page - 1);
-				            $next = ($next == $total_pag ? $total_pag : $page + 1);
-				            echo '<ul id="paginacion" class="pagination">';				            
-				            if(!($page == 1)){
-				            	echo "<li><a href=\"$_PHP_SELF?page=1\"><<</a></li>";
-				            	echo "<li><a href=\"$_PHP_SELF?page=$prev\"><</a></li>";
-				            }
-				            
-				            $i = ($page <= ($total_pag - 2) ? $page - 3 : ($page - 3) - ($page == $total_pag ? 2 : 1) );
-				            $print = 1;
-				            while ($print <= 5){
-				            	$i += 1;
-				            	if($page == $i){
-				            		echo "<li class=\"active\"><a href=\"$_PHP_SELF?page=$i\">$i <span class=\"sr-only\">(current)</span></a></li>";
-				            		$print += 1;
-				            	}elseif($i>0){
-												echo "<li><a href=\"$_PHP_SELF?page=$i\">$i</a></li>";
-												$print += 1;
-				            	}
-				            	$print = ($i >= $total_pag ? 99 : $print);
-				            }
-				            if(!($page > ($total_pag - 1)) ){
-				            	echo "<li><a href=\"$_PHP_SELF?page=$next\">></a></li>";
-				            	echo "<li><a href=\"$_PHP_SELF?page=$total_pag\">>></a></li>";
-				            }
-				            echo '</ul>';
-				            echo '</div>';
-				          ?>
-                  </tbody>
-            </table>
-        	</div>
-        	<div id="contactoPerfilContainer" class="col-md-2">
-          	Datos de un contacto
-        	</div>
-        </div>
-    </div> <!-- /container -->
-    <script type="text/javascript" src="js/servicioList.js"></script>
-  </body>
+	<div class="container">
+		<br><br><br>
+		<div class="row">
+			<h3>Contactos</h3>
+		</div>
+		<div class="row">
+			<div id="servicioListaContainer" class="col-md-2">
+				<div class="list-group"> 
+
+					<a  id="todos" class="list-group-item active">Cras justo odio</a>
+					<a  class="list-group-item" id="1">Dapibus ac facilisis in</a>
+					<a  class="list-group-item" id ="2">Morbi leo risus</a>
+					<a  class="list-group-item" id= "3">Porta ac consectetur ac</a>
+					<a  class="list-group-item" id="4">Vestibulum at eros</a>
+
+				</div> 
+			</div>
+			<div id="contactoTablaContainer" class="col-md-8">
+				<table id="tableFilter" class="table table-striped table-bordered">
+					<!--                 <table id="tableFilter" class="table table-striped table-hover table-condensed"> -->
+					<thead>
+						<tr >
+							<th>ID</th>
+							<th>NOMBRE</th>
+							<th>APELLIDO</th>
+							<th>EMAIL</th>
+							<th>SERVICIO</th>
+						</tr>
+					</thead>
+					<tbody>
+
+					</tbody>
+				</table>
+			</div>
+			<div id="contactoPerfilContainer" class="col-md-2">
+				Datos de un contacto
+			</div>
+		</div>
+	</div> <!-- /container -->
+
+	<script>
+	$(document).ready(function(){
+		$.ajax({
+			url: 'serviciosListar.php',
+			dataType: 'json',
+			success: function(serviciosList){
+				var servicioListHtml = '';
+				var len = serviciosList.length;
+				for(var i=0;i<len;i++){
+					servicioListHtml += '<a id="' + serviciosList[i].id + '" class="list-group-item">' + serviciosList[i].servicio + "</a>";
+				}
+				$('#servicioListaContainer').html(servicioListHtml);
+			}
+		});
+
+		
+		var evento1= function getServicios()
+		{
+
+			var ajaxRequest = {};
+			ajaxRequest.url = "getservicios.php";
+			ajaxRequest.type = "GET";
+			ajaxRequest.data = {servicio : $(this).attr("id")};
+			ajaxRequest.success = function(responseJSON) {
+           		 // get JSON
+           		 var servicios = responseJSON;
+           		 var tabla=""; 
+
+           		 for (var i = 0; i < servicios.length; i++) {
+           		 	tabla+="<tr>";
+           		 	tabla+="<td>"+servicios[i].id+"</td>";
+           		 	tabla+="<td>"+servicios[i].nombre+"</td>";
+           		 	tabla+="<td>"+servicios[i].apellido+"</td>";
+           		 	tabla+="<td>"+servicios[i].email+"</td>";
+           		 	tabla+="<td>"+servicios[i].servicio+"</td>";
+           		 	tabla+="</tr>";
+           		 };
+           		 $("tbody").html(tabla);
+
+           		}
+
+           		$.ajax(ajaxRequest);
+           	};
+
+           	$("a").on("click",evento1);
+           });
+</script>
+</body>
 </html>
--- getservicios.php
+++ getservicios.php
@@ -8,10 +8,21 @@
 	
 	// set MIME type
 	header("Content-type: application/json");
+	if($_GET["servicio"]!="todos")
+	{
+	$servicio = $_GET["servicio"];
+
 	$query = "select distinct (con.id), con.nombre, con.apellido, con.ci, con.email, con.telefono, con.direccion, con.observacion, ser.servicio  from contacto con 
 join contacto_servicio conser on con.id = conser.contacto 
 join servicios ser on conser.servicio = ser.id 
-join rubro ru on ser.id_rubro = ru.id"; 
+join rubro ru on ser.id_rubro = ru.id where ser.id=$servicio" ;
+	}
+	else{
+$query = "select distinct (con.id), con.nombre, con.apellido, con.ci, con.email, con.telefono, con.direccion, con.observacion, ser.servicio  from contacto con 
+join contacto_servicio conser on con.id = conser.contacto 
+join servicios ser on conser.servicio = ser.id 
+join rubro ru on ser.id_rubro = ru.id" ;
+	} 
 $resultSet = pg_query($conn,$query);
 	// -----------< construimos nuestra nhembo base de datos >-------------------//
 
